4ca0b720f043871e776b9d35ec1d77404e33d38b
Callum <callum@pixelpin.co.uk>
2017-06-13 13:35:37 +0100

    PPP-160 Removed comment block

--- wp-pixelpin-login.php
+++ wp-pixelpin-login.php
@@ -11,50 +11,6 @@ Text Domain: wordpress-pixelpin-login
 Domain Path: /languages
 */
 
-/*
-*
-*  Hi and thanks for taking the time to check out WSL code.
-*
-*  Please, don't hesitate to:
-*
-*   - Report bugs and issues.
-*   - Contribute: code, reviews, ideas and design.
-*   - Point out stupidity, smells and inconsistencies in the code.
-*   - Criticize.
-*
-*  If you want to contribute, please consider these general "guide lines":
-*
-*   - Small patches will be always welcome. Large changes should be discussed ahead of time.
-*   - That said, don't hesitate to delete code that doesn't make sense or looks redundant.
-*   - Feel free to create new functions and files when needed.
-*   - Avoid over-commenting, unless you find it necessary.
-*   - Avoid using 'switch' and 'for'. I hate those.
-*
-*  Coding Style :
-*
-*   - Readable code.
-*   - Clear indentations (tabs: 8-char indents).
-*   - Same name convention of WordPress: those long long and self-explanatory functions and variables.
-*
-*  To keep the code accessible to everyone and easy to maintain, WordPress PixelPin Login is programmed in
-*  procedural PHP and will be kept that way.
-*
-*  If you have fixed, improved or translated something in WSL, Please consider contributing back to the project
-*  by submitting a Pull Request at https://github.com/miled/wordpress-pixelpin-login
-*
-*  Grep's user, read below. Keywords stuffing:<add_action|do_action|add_filter|apply_filters>
-*
-*  If you are here just looking for the hooks, then refer to the online Developer API. If it wasn't possible to
-*  achieve some required functionality in a proper way through the already available and documented WSL hooks,
-*  please ask for support before resorting to hacks. WSL internals are not to be used.
-*  http://miled.github.io/wordpress-pixelpin-login/documentation.html
-*
-*  If you want to translate this plugin into your language (or to improve the current translations), you can
-*  join in the ongoing effort at https://www.transifex.com/projects/p/wordpress-pixelpin-login/
-*
-*  Peace.
-*
-*/
 
 // Exit if accessed directly
 if( !defined( 'ABSPATH' ) ) exit;
@@ -76,9 +32,9 @@ $_SESSION["wpl::plugin"] = "WordPress PixelPin Login " . $WORDPRESS_PIXELPIN_LOG
 
 /**
 * This file might be used to :
-*     1. Redefine WSL constants, so you can move WSL folder around.
-*     2. Define WSL Pluggable PHP Functions. See http://miled.github.io/wordpress-pixelpin-login/developer-api-functions.html
-*     5. Implement your WSL hooks.
+*     1. Redefine WPL constants, so you can move WPL folder around.
+*     2. Define WPL Pluggable PHP Functions. See http://miled.github.io/wordpress-social-login/developer-api-functions.html
+*     5. Implement your WPL hooks.
 */
 if( file_exists( WP_PLUGIN_DIR . '/wp-pixelpin-login-custom.php' ) )
 {
@@ -88,7 +44,7 @@ if( file_exists( WP_PLUGIN_DIR . '/wp-pixelpin-login-custom.php' ) )
 // --------------------------------------------------------------------
 
 /**
-* Define WSL constants, if not already defined
+* Define WPL constants, if not already defined
 */
 defined( 'WORDPRESS_PIXELPIN_LOGIN_ABS_PATH' )
 	|| define( 'WORDPRESS_PIXELPIN_LOGIN_ABS_PATH', plugin_dir_path( __FILE__ ) );
@@ -119,7 +75,7 @@ register_activation_hook( __FILE__, 'wpl_activate' );
 // --------------------------------------------------------------------
 
 /**
-* Attempt to install/migrate/repair WSL upon activation
+* Attempt to install/migrate/repair WPL upon activation
 *
 * Create wpl tables
 * Migrate old versions
@@ -231,11 +187,11 @@ function _wpl__( $text, $domain )
 
 /* includes */
 
-# WSL Setup & Settings
+# WPL Setup & Settings
 require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/settings/wpl.providers.php'            ); // List of supported providers (mostly provided by hybridauth library)
-require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/settings/wpl.database.php'             ); // Install/Uninstall WSL database tables
-require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/settings/wpl.initialization.php'       ); // Check WSL requirements and register WSL settings
-require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/settings/wpl.compatibilities.php'      ); // Check and upgrade WSL database/settings (for older versions)
+require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/settings/wpl.database.php'             ); // Install/Uninstall WPL database tables
+require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/settings/wpl.initialization.php'       ); // Check WPL requirements and register WPL settings
+require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/settings/wpl.compatibilities.php'      ); // Check and upgrade WPL database/settings (for older versions)
 
 # Services & Utilities
 require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/services/wpl.authentication.php'       ); // Authenticate users via pixelpin networks. <- that's the most important script
@@ -243,18 +199,18 @@ require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/services/wpl.mail.no
 require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/services/wpl.user.avatar.php'          ); // Display users avatar
 require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/services/wpl.user.data.php'            ); // User data functions (database related)
 require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/services/wpl.utilities.php'            ); // Unclassified functions & utilities
-require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/services/wpl.watchdog.php'             ); // WSL logging agent
+require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/services/wpl.watchdog.php'             ); // WPL logging agent
 
-# WSL Widgets & Front-end interfaces
-require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/widgets/wpl.auth.widgets.php'          ); // Authentication widget generators (where WSL widget/icons are displayed)
+# WPL Widgets & Front-end interfaces
+require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/widgets/wpl.auth.widgets.php'          ); // Authentication widget generators (where WPL widget/icons are displayed)
 require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/widgets/wpl.users.gateway.php'         ); // Accounts linking + Profile Completion
-require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/widgets/wpl.error.pages.php'           ); // Generate WSL notices end errors pages
-require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/widgets/wpl.loading.screens.php'       ); // Generate WSL loading screens
+require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/widgets/wpl.error.pages.php'           ); // Generate WPL notices end errors pages
+require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/widgets/wpl.loading.screens.php'       ); // Generate WPL loading screens
 
-# WSL Admin interfaces
+# WPL Admin interfaces
 if( is_admin() && ( !defined( 'DOING_AJAX' ) || !DOING_AJAX ) )
 {
-	require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/admin/wpl.admin.ui.php'        ); // The entry point to WSL Admin interfaces
+	require_once( WORDPRESS_PIXELPIN_LOGIN_ABS_PATH . 'includes/admin/wpl.admin.ui.php'        ); // The entry point to WPL Admin interfaces
 }
 
 // --------------------------------------------------------------------
